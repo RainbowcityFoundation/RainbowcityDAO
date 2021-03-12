28554a7ee52117708dfe3fdf00bbb438b56ee22c
sylvanus <sylvanuss@126.com>
2021-03-12 17:26:15 +0800

    “addnewmining”

---
+++ contracts/mining/RbContribution.sol
@@ -0,0 +1,11 @@
+pragma solidity =0.8.0;
+
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+import "../interface/mining/IRbConsensus.sol";
+import "../lib/TransferHelper.sol";
+import "./MiningBase.sol";
+//贡献挖矿
+
+contract RbContribution is MiningBase {
+
+}
